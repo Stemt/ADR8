2259eb7b7f4108e13e899559c3ad2a8ea17a6cff
Alaric de Ruiter <alaric0605@gmail.com>
2024-04-24 08:38:59 +0200

    removed duplicate code from hello world assembly program

--- examples/hello_world.asm
+++ examples/hello_world.asm
@@ -1,21 +1,3 @@
-// when loading using the program loader this part is required to make sure
-// that the program loader stays intact while overwriting itself
-// !! PROGRAM LOADER DO NOT CHANGE !!
-ENTRY:
-  LDAL 0x1000
-  LDAH 0x1000
-  SETY 0x0000
-
-LOAD_PROGRAM:
-  LDBL 0x1000
-  SYBL
-  INCY
-  DEC
-  SETB 0x0000
-  JGTA LOAD_PROGRAM
-  JMPA PROGRAM_ENTRY
-// !! PROGRAM LOADER DO NOT CHANGE !!
-
 STRING: "hello world!\n"
 
 PROGRAM_ENTRY:
